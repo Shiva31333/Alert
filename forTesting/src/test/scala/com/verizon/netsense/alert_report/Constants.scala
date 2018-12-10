405458cec4a50df086513def8490f3fb17d383ac
Shiva31333 <45457331+Shiva31333@users.noreply.github.com>
2018-12-10 10:20:39 -0500

    Add files via upload

---
+++ forTesting/src/test/scala/com/verizon/netsense/alert_report/Constants.scala
@@ -0,0 +1,133 @@
+package com.verizon.netsense.alert_report
+
+import java.util
+
+object Constants {
+
+  val DELETEREQUESTMETHOD = "DELETE"
+  val GETREQUESTMETHOD = "GET"
+  val PUTREQUESTMETHOD = "PUT"
+  val POSTREQUESTMETHOD = "POST"
+  val REQUESTHEADERS =
+    """
+      |"Accept": "*/*",
+      |"accept-encoding": "gzip, deflate",
+      |"Content-Type": "application/json",
+      |"Host": "nsn-lcodev.sensity.com",
+      |"User-Agent": "PostmanRuntime/7.4.0",
+      |"X-Amzn-Trace-Id": "Root=1-5c05c63d-dbfcfbecfd0be35431fc25b8",
+      |"X-Forwarded-For": "68.128.164.93",
+      |"X-Forwarded-Port": "443",
+      |"X-Forwarded-Proto": "https"
+    """.stripMargin
+
+  val EMPTYREQUESTBODY =
+    """{
+      |}
+    """.stripMargin
+
+  val REQUESTbODY =
+    """
+      |{
+      |  "reportName": "MyReport",
+      |  "reportType": "Alert Summary Report",
+      |  "timePeriod": "1day",
+      |  "active": true,
+      |  "recipients": [{"userName": "Lakshman", "userEmail": "lakshman.kolliapra@hcl.com"}, {"userName": "John Wood", "userEmail": "john.wood@hcl.com"}],
+      |  "rules": "{\"nodeids\":[\"N1\", \"N2\", \"N3\"],\"alarmtypes\": [\"T1\", \"T2\", \"T3\"],\"alertstatus\": \"active\",\"reportcolumns\": [\"nodeid\", \"alarmtype\"]}"
+      |}
+    """.stripMargin
+
+  val REQUESTbODY_backup =
+    """
+      |"reportId":"12345",
+      |"reportType":"Test",
+      |"siteId" :"siteId",
+      |"orgId":"orgId",
+      |"active":"TRUE"
+    """.stripMargin
+  var REQUESTHEADER: java.util.Map[String, String] = new util.HashMap();
+  var PATHPARAMETERSREQUESTBODY: java.util.Map[String, String] = new util.HashMap();
+
+  val RequestString =
+    """{
+      |    "resource": "/",
+      |    "path": "/sensity",
+      |    "httpMethod": "POST",
+      |    "headers": {
+      |        "Accept": "*/*",
+      |        "accept-encoding": "gzip, deflate",
+      |        "Content-Type": "application/json",
+      |        "Host": "nsn-lcodev.sensity.com",
+      |        "User-Agent": "PostmanRuntime/7.4.0",
+      |        "X-Amzn-Trace-Id": "Root=1-5c05c63d-dbfcfbecfd0be35431fc25b8",
+      |        "X-Forwarded-For": "68.128.164.93",
+      |        "X-Forwarded-Port": "443",
+      |        "X-Forwarded-Proto": "https"
+      |    },
+      |    "multiValueHeaders": {
+      |        "Accept": [
+      |            "*/*"
+      |        ],
+      |        "accept-encoding": [
+      |            "gzip, deflate"
+      |        ],
+      |        "Content-Type": [
+      |            "application/json"
+      |        ],
+      |        "Host": [
+      |            "nsn-lcodev.sensity.com"
+      |        ],
+      |        "User-Agent": [
+      |            "PostmanRuntime/7.4.0"
+      |        ],
+      |        "X-Amzn-Trace-Id": [
+      |            "Root=1-5c05c63d-dbfcfbecfd0be35431fc25b8"
+      |        ],
+      |        "X-Forwarded-For": [
+      |            "68.128.164.93"
+      |        ],
+      |        "X-Forwarded-Port": [
+      |            "443"
+      |        ],
+      |        "X-Forwarded-Proto": [
+      |            "https"
+      |        ]
+      |    },
+      |    "queryStringParameters": null,
+      |    "multiValueQueryStringParameters": null,
+      |    "pathParameters": null,
+      |    "stageVariables": null,
+      |    "requestContext": {
+      |        "resourceId": "gt1hpp84c2",
+      |        "resourcePath": "/",
+      |        "httpMethod": "POST",
+      |        "extendedRequestId": "RWvppGHFvHcFzMQ=",
+      |        "requestTime": "04/Dec/2018:00:11:41 +0000",
+      |        "path": "/sensity",
+      |        "accountId": "173021575303",
+      |        "protocol": "HTTP/1.1",
+      |        "stage": "lco_dev_test",
+      |        "domainPrefix": "nsn-lcodev",
+      |        "requestTimeEpoch": 1543882301713,
+      |        "requestId": "2d30e891-f759-11e8-9324-b5f0d3b0cbcd",
+      |        "identity": {
+      |            "cognitoIdentityPoolId": null,
+      |            "accountId": null,
+      |            "cognitoIdentityId": null,
+      |            "caller": null,
+      |            "sourceIp": "68.128.164.93",
+      |            "accessKey": null,
+      |            "cognitoAuthenticationType": null,
+      |            "cognitoAuthenticationProvider": null,
+      |            "userArn": null,
+      |            "userAgent": "PostmanRuntime/7.4.0",
+      |            "user": null
+      |        },
+      |        "domainName": "nsn-lcodev.sensity.com",
+      |        "apiId": "5bw3bgmlg3"
+      |    },
+      |    "body": "{\"reportId\":\"12345\", \"reportType\":\"Test\", \"siteId\":\"siteid1\", \"orgId\": \"orgid1\", \"active\" : \"true\"}",
+      |    "isBase64Encoded": false
+      |}""".stripMargin
+}
--- forTesting/src/test/scala/com/verizon/netsense/alert_report/ProxyRequestMainTest.scala
+++ forTesting/src/test/scala/com/verizon/netsense/alert_report/ProxyRequestMainTest.scala
@@ -1,118 +1,253 @@
 package com.verizon.netsense.alert_report
 
-import com.verizon.netsense.alert_report.utils.ObjectMapperUtil
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent
+import com.verizon.netsense.alert_report.Constants.{PATHPARAMETERSREQUESTBODY, REQUESTHEADER}
+import com.verizon.netsense.alert_report.aws_lambda.ProxyRequestMain
+import com.verizon.netsense.alert_report.utils.ObjectMapperUtil.fromJson
+import com.verizon.netsense.alert_report.utils.{RawReport, Report}
+import org.scalatest.{BeforeAndAfterEach, FlatSpec, Matchers}
 
-import org.scalatest.{BeforeAndAfterEach, FunSuite}
-
-class ProxyRequestMainTest extends FunSuite with BeforeAndAfterEach {
+class ProxyRequestMainTest extends FlatSpec with BeforeAndAfterEach with Matchers {
 
 
   private var subject = null
   private var testContext = null
 
+  var request = new APIGatewayProxyRequestEvent();
+  request.setResource("/")
+  request.setPath("/")
+  REQUESTHEADER.put("Accept", "*/*")
+  REQUESTHEADER.put("accept-encoding", "gzip) deflate")
+  REQUESTHEADER.put("Content-Type", "application/json")
+  REQUESTHEADER.put("Host", "nsn-lcodev.sensity.com")
+  REQUESTHEADER.put("User-Agent", "PostmanRuntime/7.4.0")
+  REQUESTHEADER.put("X-Amzn-Trace-Id", "Root=1-5c05c63d-dbfcfbecfd0be35431fc25b8")
+  REQUESTHEADER.put("X-Forwarded-For", "68.128.164.93")
+  REQUESTHEADER.put("X-Forwarded-Port", "443")
+  REQUESTHEADER.put("X-Forwarded-Proto", "https")
 
-  test("testHandleRequest") {
+  it should "testing createReport success case" in {
 
+    request.setHeaders(REQUESTHEADER)
+    request.setBody(Constants.REQUESTbODY)
+    request.setHttpMethod(Constants.POSTREQUESTMETHOD)
+    PATHPARAMETERSREQUESTBODY.put("reportid", "")
+    PATHPARAMETERSREQUESTBODY.put("customerid", "")
+    PATHPARAMETERSREQUESTBODY.put("siteid", "")
+    request.setPathParameters(PATHPARAMETERSREQUESTBODY)
 
-    val str = """{
-                |    "resource": "/",
-                |    "path": "/sensity",
-                |    "httpMethod": "POST",
-                |    "headers": {
-                |        "Accept": "*/*",
-                |        "accept-encoding": "gzip, deflate",
-                |        "Content-Type": "application/json",
-                |        "Host": "nsn-lcodev.sensity.com",
-                |        "User-Agent": "PostmanRuntime/7.4.0",
-                |        "X-Amzn-Trace-Id": "Root=1-5c05c63d-dbfcfbecfd0be35431fc25b8",
-                |        "X-Forwarded-For": "68.128.164.93",
-                |        "X-Forwarded-Port": "443",
-                |        "X-Forwarded-Proto": "https"
-                |    },
-                |    "multiValueHeaders": {
-                |        "Accept": [
-                |            "*/*"
-                |        ],
-                |        "accept-encoding": [
-                |            "gzip, deflate"
-                |        ],
-                |        "Content-Type": [
-                |            "application/json"
-                |        ],
-                |        "Host": [
-                |            "nsn-lcodev.sensity.com"
-                |        ],
-                |        "User-Agent": [
-                |            "PostmanRuntime/7.4.0"
-                |        ],
-                |        "X-Amzn-Trace-Id": [
-                |            "Root=1-5c05c63d-dbfcfbecfd0be35431fc25b8"
-                |        ],
-                |        "X-Forwarded-For": [
-                |            "68.128.164.93"
-                |        ],
-                |        "X-Forwarded-Port": [
-                |            "443"
-                |        ],
-                |        "X-Forwarded-Proto": [
-                |            "https"
-                |        ]
-                |    },
-                |    "queryStringParameters": null,
-                |    "multiValueQueryStringParameters": null,
-                |    "pathParameters": null,
-                |    "stageVariables": null,
-                |    "requestContext": {
-                |        "resourceId": "gt1hpp84c2",
-                |        "resourcePath": "/",
-                |        "httpMethod": "POST",
-                |        "extendedRequestId": "RWvppGHFvHcFzMQ=",
-                |        "requestTime": "04/Dec/2018:00:11:41 +0000",
-                |        "path": "/sensity",
-                |        "accountId": "173021575303",
-                |        "protocol": "HTTP/1.1",
-                |        "stage": "lco_dev_test",
-                |        "domainPrefix": "nsn-lcodev",
-                |        "requestTimeEpoch": 1543882301713,
-                |        "requestId": "2d30e891-f759-11e8-9324-b5f0d3b0cbcd",
-                |        "identity": {
-                |            "cognitoIdentityPoolId": null,
-                |            "accountId": null,
-                |            "cognitoIdentityId": null,
-                |            "caller": null,
-                |            "sourceIp": "68.128.164.93",
-                |            "accessKey": null,
-                |            "cognitoAuthenticationType": null,
-                |            "cognitoAuthenticationProvider": null,
-                |            "userArn": null,
-                |            "userAgent": "PostmanRuntime/7.4.0",
-                |            "user": null
-                |        },
-                |        "domainName": "nsn-lcodev.sensity.com",
-                |        "apiId": "5bw3bgmlg3"
-                |    },
-                |    "body": "{\"reportId\":\"12345\", \"reportType\":\"Test\", \"siteId\":\"siteid1\", \"orgId\": \"orgid1\", \"active\" : \"true\"}",
-                |    "isBase64Encoded": false
-                |}""".stripMargin
+    println(s" createReport body = ${request.getBody}")
+    println(s" createReport headers = ${request.getHeaders}")
 
+    val response = ProxyRequestMain.handleRequest(request, testContext)
+    val reportPayload: Report = Report.apply(fromJson[RawReport](response.getBody), "", "")
 
+    assert(response.getStatusCode.equals(200) && reportPayload.orgId.equals(PATHPARAMETERSREQUESTBODY.get("orgId")) &&
+      reportPayload.siteId.equals(PATHPARAMETERSREQUESTBODY.get("siteid")))
+  }
 
-    var inputStream = new java.io.ByteArrayInputStream(str.getBytes)
+  it should "testing createReport failure case - path parameters empty" in {
 
+    request.setHeaders(REQUESTHEADER)
+    request.setBody(Constants.REQUESTbODY)
+    request.setHttpMethod(Constants.POSTREQUESTMETHOD)
+    PATHPARAMETERSREQUESTBODY.put("reportid", "")
+    PATHPARAMETERSREQUESTBODY.put("customerid", "")
+    PATHPARAMETERSREQUESTBODY.put("siteid", "")
+    request.setPathParameters(PATHPARAMETERSREQUESTBODY)
 
+    println(s" createReport body = ${request.getBody}")
+    println(s" createReport headers = ${request.getHeaders}")
 
+    val response = ProxyRequestMain.handleRequest(request, testContext)
+    val reportPayload: Report = Report.apply(fromJson[RawReport](response.getBody), "", "")
 
+    assert(response.getStatusCode.equals(400) && reportPayload.orgId.equals(PATHPARAMETERSREQUESTBODY.get("orgId")) &&
+      reportPayload.siteId.equals(PATHPARAMETERSREQUESTBODY.get("siteid")))
+  }
 
-    val request = Handler.convertInputStreamToReq(inputStream)
+  it should "testing delete report success case" in {
 
-    println(s"body = ${request.getBody}")
-    println(s"headers = ${request.getHeaders}")
+    request.setHttpMethod(Constants.DELETEREQUESTMETHOD)
 
+    PATHPARAMETERSREQUESTBODY.put("reportid", "")
+    PATHPARAMETERSREQUESTBODY.put("customerid", "")
+    PATHPARAMETERSREQUESTBODY.put("siteid", "")
 
-    val getProxyResponse = ProxyRequestMain.handleRequest(request, testContext)
+    request.setBody(Constants.EMPTYREQUESTBODY)
+    request.setPathParameters(PATHPARAMETERSREQUESTBODY)
+
+    println(s" delete report body = ${request.getBody}")
+    println(s" delete report headers = ${request.getHeaders}")
+    val response = ProxyRequestMain.handleRequest(request, testContext)
+    assert(response.getStatusCode.equals(200) && response.getBody.equals(s"Report " + PATHPARAMETERSREQUESTBODY.get("reportid") + " deleted successfully"))
+  }
+
+  it should "testing delete report failure case - invalid report id" in {
+
+    request.setHttpMethod(Constants.DELETEREQUESTMETHOD)
+
+    PATHPARAMETERSREQUESTBODY.put("reportid", "")
+    PATHPARAMETERSREQUESTBODY.put("customerid", "")
+    PATHPARAMETERSREQUESTBODY.put("siteid", "")
+
+    request.setBody(Constants.EMPTYREQUESTBODY)
+    request.setPathParameters(PATHPARAMETERSREQUESTBODY)
+
+    println(s" delete report body = ${request.getBody}")
+    println(s" delete report headers = ${request.getHeaders}")
+    val response = ProxyRequestMain.handleRequest(request, testContext)
+    assert(response.getStatusCode.equals(404) && response.getBody.equals(s"No Report found for reportId: " + PATHPARAMETERSREQUESTBODY.get("reportid")))
+  }
+
+  it should "testing get only one report record success case- reportid,orgid,siteid" in {
+
+    request.setHttpMethod(Constants.GETREQUESTMETHOD)
+
+    PATHPARAMETERSREQUESTBODY.put("reportid", "1")
+    PATHPARAMETERSREQUESTBODY.put("customerid", "123")
+    PATHPARAMETERSREQUESTBODY.put("siteid", "1")
+
+    request.setBody(Constants.EMPTYREQUESTBODY)
+    request.setPathParameters(PATHPARAMETERSREQUESTBODY)
+
+    println(s" get report record body = ${request.getBody}")
+    println(s" get report record  headers = ${request.getHeaders}")
+    val response = ProxyRequestMain.handleRequest(request, testContext)
+    val reportPayload: Report = Report.apply(fromJson[RawReport](response.getBody), "", "")
+
+    assert(response.getStatusCode.equals(200) && reportPayload.orgId.equals(PATHPARAMETERSREQUESTBODY.get("orgId")) &&
+      reportPayload.siteId.equals(PATHPARAMETERSREQUESTBODY.get("siteid")))
+  }
+
+  it should "testing get all report records success case" in {
+
+    request.setHttpMethod(Constants.GETREQUESTMETHOD)
+
+    PATHPARAMETERSREQUESTBODY.put("customerid", "")
+    PATHPARAMETERSREQUESTBODY.put("siteid", "")
+
+    request.setBody(Constants.EMPTYREQUESTBODY)
+    request.setPathParameters(PATHPARAMETERSREQUESTBODY)
+
+    println(s" get all report records body = ${request.getBody}")
+    println(s" get all report records headers = ${request.getHeaders}")
+
+    val response = ProxyRequestMain.handleRequest(request, testContext)
+    val reportPayload: Report = Report.apply(fromJson[RawReport](response.getBody), "", "")
+
+    assert(response.getStatusCode.equals(200) && reportPayload.orgId.equals(PATHPARAMETERSREQUESTBODY.get("orgId")) &&
+      reportPayload.siteId.equals(PATHPARAMETERSREQUESTBODY.get("siteid")))
+  }
+
+  it should "testing activate report record success case" in {
+
+    request.setHttpMethod(Constants.PUTREQUESTMETHOD)
+
+    PATHPARAMETERSREQUESTBODY.put("reportid", "")
+    PATHPARAMETERSREQUESTBODY.put("customerid", "")
+    PATHPARAMETERSREQUESTBODY.put("siteid", "")
+
+    request.setBody(Constants.EMPTYREQUESTBODY)
+    request.setPathParameters(PATHPARAMETERSREQUESTBODY)
+
+    println(s" activate report body = ${request.getBody}")
+    println(s" activate report headers = ${request.getHeaders}")
+    val response = ProxyRequestMain.handleRequest(request, testContext)
+    assert(response.getStatusCode.equals(200) && response.getBody.equals(s"Report " + PATHPARAMETERSREQUESTBODY.get("reportid") + " activated successfully"))
+  }
+
+  it should "testing activate report record failure case - report id not found" in {
+
+    request.setHttpMethod(Constants.PUTREQUESTMETHOD)
+
+    PATHPARAMETERSREQUESTBODY.put("reportid", "")
+    PATHPARAMETERSREQUESTBODY.put("customerid", "")
+    PATHPARAMETERSREQUESTBODY.put("siteid", "")
+
+    request.setBody(Constants.EMPTYREQUESTBODY)
+    request.setPathParameters(PATHPARAMETERSREQUESTBODY)
+
+    println(s" activate report body = ${request.getBody}")
+    println(s" activate report headers = ${request.getHeaders}")
+    val response = ProxyRequestMain.handleRequest(request, testContext)
+    assert(response.getStatusCode.equals(404) && response.getBody.equals(s"No Report found for reportId: " + PATHPARAMETERSREQUESTBODY.get("reportid")))
+  }
+
+  it should "testing deActivate report record success case" in {
+
+    request.setHttpMethod(Constants.PUTREQUESTMETHOD)
+
+    PATHPARAMETERSREQUESTBODY.put("reportid", "")
+    PATHPARAMETERSREQUESTBODY.put("customerid", "")
+    PATHPARAMETERSREQUESTBODY.put("siteid", "")
+
+    request.setBody(Constants.EMPTYREQUESTBODY)
+    request.setPathParameters(PATHPARAMETERSREQUESTBODY)
+
+    println(s" deActivate report record body = ${request.getBody}")
+    println(s" deActivate report record headers = ${request.getHeaders}")
+    val response = ProxyRequestMain.handleRequest(request, testContext)
+    assert(response.getStatusCode.equals(200) && response.getBody.equals(s"Report " + PATHPARAMETERSREQUESTBODY.get("reportid") + " de-activated successfully"))
+
+  }
+  it should "testing deActivate report record failure case - report id not found" in {
+
+    request.setHttpMethod(Constants.PUTREQUESTMETHOD)
+
+    PATHPARAMETERSREQUESTBODY.put("reportid", "")
+    PATHPARAMETERSREQUESTBODY.put("customerid", "")
+    PATHPARAMETERSREQUESTBODY.put("siteid", "")
+
+    request.setBody(Constants.EMPTYREQUESTBODY)
+    request.setPathParameters(PATHPARAMETERSREQUESTBODY)
+
+    println(s" deActivate report record body = ${request.getBody}")
+    println(s" deActivate report record headers = ${request.getHeaders}")
+    val response = ProxyRequestMain.handleRequest(request, testContext)
+    assert(response.getStatusCode.equals(200) && response.getBody.equals(s"No Report found for reportId: " + PATHPARAMETERSREQUESTBODY.get("reportid")))
 
   }
 
+  it should "testing update report record success case" in {
 
+    request.setHttpMethod(Constants.PUTREQUESTMETHOD)
+
+    PATHPARAMETERSREQUESTBODY.put("reportid", "")
+    PATHPARAMETERSREQUESTBODY.put("customerid", "")
+    PATHPARAMETERSREQUESTBODY.put("siteid", "")
+
+    request.setBody(Constants.EMPTYREQUESTBODY)
+    request.setPathParameters(PATHPARAMETERSREQUESTBODY)
+
+    println(s" update report record body = ${request.getBody}")
+    println(s" update report record headers = ${request.getHeaders}")
+
+    val response = ProxyRequestMain.handleRequest(request, testContext)
+    val reportPayload: RawReport = fromJson[RawReport](response.getBody)
+
+    assert(response.getStatusCode.equals(200))
+  }
+
+  it should "testing update report record failure case - report id not found" in {
+
+    request.setHttpMethod(Constants.PUTREQUESTMETHOD)
+
+    PATHPARAMETERSREQUESTBODY.put("reportid", "")
+    PATHPARAMETERSREQUESTBODY.put("customerid", "")
+    PATHPARAMETERSREQUESTBODY.put("siteid", "")
+
+    request.setBody(Constants.EMPTYREQUESTBODY)
+    request.setPathParameters(PATHPARAMETERSREQUESTBODY)
+
+    println(s" update report record body = ${request.getBody}")
+    println(s" update report record headers = ${request.getHeaders}")
+
+    val response = ProxyRequestMain.handleRequest(request, testContext)
+    val reportPayload: RawReport = fromJson[RawReport](response.getBody)
+
+    assert(response.getStatusCode.equals(404) && response.getBody.equals(s"No Report found for reportId: ${PATHPARAMETERSREQUESTBODY.get("reportid")}"))
+  }
 
 }
+
